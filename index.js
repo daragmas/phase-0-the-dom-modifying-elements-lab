4ddec9b299f521d4ab444315c766b3df17f3f1f4
daragmas <daragmas@gmail.com>
2022-06-13 14:43:52 -0400

    commit tree 7380f542c5b092c992790d1b701377f02ca84d52

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement('h1');
+newHeader.textContent = 'Nate is the champion';
+newHeader.id = 'victory';
+newHeader.className = 'victory';
